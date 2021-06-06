55889ce63cb42fb388f46dd6000215e5c31b1f72
Camila <cruz.camila001@gmail.com>
2021-06-06 18:30:26 -0300

    feat: add header component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+import Header from '../src/commons/Header';
 
 export default function Home() {
-  return <Title>My page</Title>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
